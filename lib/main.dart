dd5b0da80c1d83b6f8df28a846a02f7d8dc7d0b5
Magatte-lo <papemagattelo90@gmail.com>
2026-05-19 15:43:42 +0000

    chore: initial Flutter project setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'app.dart';
+import 'firebase_options.dart';
 
 void main() async {
-  // Assure que Flutter est initialisé avant tout (Firebase plus tard)
+  // Obligatoire avant tout await dans main()
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialisation de Firebase avec la config générée
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(
     const ProviderScope(
